990e218e62ac71ded7b78374c1713294da2e8e3c
avrymi-asraf <avizmzm49@gmail.com>
2021-08-15 12:14:33 +0300

    combain files and test chain function

--- chain_rule.py
+++ chain_rule.py
@@ -9,6 +9,66 @@ import pandas as pd
 from functions_dereative_nested import *
 
 
+a = np.array([r.randint(-10, 10) for i in range(20)])
+b = np.array([r.randint(-10, 10) for i in range(20)])
+a * b
+
+tow_d = np.array([a, b])
+tow_d.sum(axis=0)
+tow_d.sum(axis=1)
+
+
+def square(x: np.ndarray) -> np.ndarray:
+    """square every elemnet in ndarrauy"""
+    return np.power(x, 2)
+
+
+def leaky_relu(x: np.ndarray) -> np.ndarray:
+    """Apply "Leaky ReLU" function to each element in ndarray"""
+    return np.maximum(0.2 * x, x)
+
+
+#%%
+# defined types
+Array_function = Callable[[np.ndarray], np.ndarray]
+Chain = List[Array_function]
+
+
+
+
+def deriv(
+    func: Callable[[np.ndarray], np.ndarray], input_: np.ndarray, delta: float = 0.001
+) -> np.ndarray:
+    """return the deriative of function of every point in input array
+
+    Args:
+        func (Callable[[np.ndarray], np.ndarray]): function exspres by ndarray
+        input (np.ndarray): point to calculte the derivative
+        delta (float, optional): value of delte for moast function is enofe. Defaults to 0.001.
+
+    Returns:
+        np.ndarray: the derivative in points
+    """
+    return (func(input_ + delta) - func(input_)) / (2 * delta)
+
+
+#%%
+
+def chain_length_2(chain: Chain, a: np.ndarray) -> np.ndarray:
+    """chain two functins
+
+    Args:
+        chain (Chain): list of ndarray functions
+        a (np.ndarray): parameter to pass in functions
+
+    Returns:
+        np.ndarray:
+    """
+    assert len(chain) == 2, "The chain shuld be 2"
+    f1 = chain[0]
+    f2 = chain[1]
+    return f1(f2(a))
+
 # defined types
 Array_function = Callable[[np.ndarray], np.ndarray]
 Chain = List[Array_function]
@@ -52,8 +112,6 @@ def chain_deriv_2(chain: Chain, input_range: np.ndarray) -> np.ndarray:
 
 PLOT_RANGE = np.arange(-3, 3, 0.01)
 def chain(d): return chain_length_2([square, sigmoid], d)
-
-
 def chain_der(d): return chain_deriv_2([square, sigmoid], d)
 
 
@@ -64,8 +122,6 @@ sns.lineplot(x=PLOT_RANGE, y=chain_der(PLOT_RANGE))
 
 
 # %%
-
-
 def chain_deriv_3(chain: Chain, input_range: np.ndarray) -> np.ndarray:
     """calcult deriv for 3 functions 
 
@@ -104,6 +160,16 @@ def chain_deriv_3(chain: Chain, input_range: np.ndarray) -> np.ndarray:
 
 
 def rection_der(chain: Chain, input_range: np.ndarray,ind_chain:int) -> np.ndarray:
+    """calcult derivative of chain function 
+
+    Args:
+        chain (Chain):list of function
+        input_range (np.ndarray): input range to calcult 
+        ind_chain (int): index of function
+
+    Returns:
+        np.ndarray: derivative of 
+    """
     if ind_chain == len(chain)-1:
         f_x = chain[ind_chain](input_range)
         return deriv(chain[ind_chain],input_range)
@@ -112,7 +178,17 @@ def rection_der(chain: Chain, input_range: np.ndarray,ind_chain:int) -> np.ndarr
     
 
 
+# %% plot 
+chain_of_3 = [leaky_relu,square,sigmoid]
+x = PLOT_RANGE
+y = chain_deriv_3(chain_of_3,PLOT_RANGE)
+sns.lineplot(x,y)
+
+
+
 # %%
-
-
-
+chain_of_3 = [leaky_relu,square,sigmoid]
+x = PLOT_RANGE
+y = chain_deriv_3(chain_of_3,PLOT_RANGE)
+rection_der(chain_of_3,PLOT_RANGE,0)
+sns.lineplot(x,y)
